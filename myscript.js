db7fb35bced40b9ea5f855726f43c601664f9455
Zeyad Etman <zeyadetman@yahoo.com>
2017-09-14 14:50:34 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 setInterval(function() {
     var notcounter = document.getElementsByClassName("count-inner")[0].innerHTML * 1 + document.getElementsByClassName("count-inner")[1].innerHTML * 1;
-    console.log(notcounter);
+    //console.log(notcounter);
     if (notcounter > 0) {
         if (confirm(`${notcounter} unread notifications on twitter! click OK if you wanna mark as read :)`)) {
             document.getElementsByClassName("count-inner")[0].innerHTML = "";
